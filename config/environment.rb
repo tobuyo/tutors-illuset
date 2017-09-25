140c6c800809a851451dd8b9e883cfac745e0be6
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-09-25 13:34:58 +0900

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => 'smtp.gmail.com',
-  :user_name => "tutors.test7@gmail.com", #gmailアドレス
-  :password => "", #gmailパスワード
-  :authentication => 'login',
-}
