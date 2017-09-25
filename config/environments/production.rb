cde715f89a97a305800df7f43962b0258537003f
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-09-25 13:50:06 +0900

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'appname.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://tutors-illust.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :domain => 'smtp.gmail.com',
+      :user_name => 'tutors.test7@gmail.com',
+      :password => 'toraifannzu',
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
