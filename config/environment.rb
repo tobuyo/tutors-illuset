1febd7b4d45bd6d2fe7ba72f121d0ba854f85600
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-09-25 14:32:08 +0900

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app77581031@heroku.com'],
+  :password       => ENV['ishimaru7'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
