00a486d49ee338cfc8ec261b5cf94f3bd147b4d3
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-09-27 15:39:23 +0900

    localでの再現に成功　userのアイコン

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,24 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'ap-northeaset-1'
+    region: 'ap-northeast-1'
   }
 
-    case Rails.env
-    when 'development'
-        config.fog_directory  = 'tutors-upload-test'
-        config.asset_host = 'https://s3.amazonaws.com/tutors-upload-test'
+  case Rails.env
     when 'production'
-        config.fog_directory  = 'tutors-upload-test'
-        config.asset_host = 'https://s3.amazonaws.com/tutors-upload-test'
-    end
+      config.fog_directory = 'tutors-upload-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tutors-upload-test'
+
+    when 'development'
+      config.fog_directory = 'tutors-upload-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tutors-upload-test'
+
+    when 'test'
+      config.fog_directory = 'tutors-upload-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tutors-upload-test'
+  end
 end
