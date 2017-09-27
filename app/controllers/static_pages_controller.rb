5cf157c63bdfee47981a82300859e21898685c43
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-09-27 19:42:35 +0900

    home画面を変更、lessonがeachで順次表示されるように

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @lessons = Lesson.all
+  end
+
+  private
+  
+  def lesson_params
+    params.require(:lesson).permit(:user_id, :title, :price, :simple_description, :detail_description)
   end
 end
