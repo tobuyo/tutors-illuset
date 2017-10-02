eaad57c07a479f21ef1d582612ef2f9ae2c6c6eb
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-10-02 10:09:51 +0900

    各ユーザー画面にレッスンを表示させ中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
 
   def home
     @lessons = Lesson.all
-
     @reports = Lesson.all.order(:title)
   end
 
@@ -12,5 +11,9 @@ class StaticPagesController < ApplicationController
   
   def lesson_params
     params.require(:lesson).permit(:user_id, :title, :price, :simple_description, :detail_description)
+  end
+
+  def user_params
+    params.require(:user).permit(:user_id, :username)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,13 @@ class UsersController < ApplicationController
  
   def show
     @user = User.find(params[:id])
+    @lessons = Lesson.find(params[:user_id])
+    binding.pry
+  end
+
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:header, :user_id, :title, :price, :simple_description, :detail_description)
   end
 end
