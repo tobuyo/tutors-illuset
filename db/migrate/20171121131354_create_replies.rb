3283a5b5ab4c479f6584b41219ad438d97ef3afc
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-11-25 15:54:48 +0900

    replyimage

--- db/migrate/20171121131354_create_replies.rb
+++ db/migrate/20171121131354_create_replies.rb
@@ -4,6 +4,7 @@ class CreateReplies < ActiveRecord::Migration[5.1]
       t.integer :user_id
       t.integer :comment_id
       t.text :body
+      t.string :replyimage, null: true
       t.timestamps
     end
   end
--- db/migrate/20171123092507_add_replyimage_to_replies.rb
+++
@@ -1,5 +0,0 @@
-class AddReplyimageToReplies < ActiveRecord::Migration[5.1]
-  def change
-    add_column :replies, :replyimage, :string
-  end
-end
